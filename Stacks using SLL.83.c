5a17275977197d449cde6f7b0f7d3c153c04bc95
syedaamara26 <syedaamara26@gmail.com>
2026-04-26 05:43:16 +0530

    Add files via upload

---
+++ Stacks using SLL.83.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+int data;
+struct Node * next;
+};
+struct Node * head = NULL, *temp;
+void push (int x)
+{
+temp=(struct Node*)malloc(sizeof(struct Node));
+temp->data = x;
+temp->next = head;
+head = temp;
+}
+void pop()
+{
+if (head == NULL)
+{
+printf("Stack Underflow\n");
+return;}
+temp = head;
+printf("Deleted:%d\n", temp->data);
+head = head->next;
+free(temp);
+}
+void display()
+{
+struct Node *current = head;
+while(current!= NULL)
+{
+printf("%d->", current->data);
+current = current->next;
+} printf("NULL\n");
+}
+int main()
+{int choice, x;
+while(1)
+{
+printf("\n---Stack Using SLL---\n");
+printf("1. Push\n 2. Pop\n 3. Display\n 4.Exit\n");
+scanf("%d", &choice);
+switch(choice)
+{
+case 1:
+printf("Enter data:");
+scanf("%d", &x);
+push(x);
+break;
+case 2:
+pop();
+break;
+case 3:
+display();
+break;
+case 4:
+exit (0);
+}}}
