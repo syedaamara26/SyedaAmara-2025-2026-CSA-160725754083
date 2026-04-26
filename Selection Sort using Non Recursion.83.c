348d45e1faf261f821abff366d3427a7b49b408f
syedaamara26 <syedaamara26@gmail.com>
2026-04-26 05:56:07 +0530

    Add files via upload

---
+++ Selection Sort using Non Recursion.83.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int a[10], n, i, j, min, temp;
+printf("Enter n:");
+scanf("%d", &n);
+printf("Enter elements:\n");
+for(i=0;i<n;i++)
+scanf("%d", &a[i]);
+for(i=0; i<n-1;i++){
+min = i;
+for(j=i+1;j<n;j++){
+if(a[j]<a[min])
+min=j;}
+//swap
+temp=a[i];
+a[i]=a[min];
+a[min]=temp;}
+printf("Sorted Array:\n");
+for(i=0; i<n;i++)
+printf("%d\n", a[i]);
+return 0;
+}
